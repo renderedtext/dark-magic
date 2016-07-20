02ad6ed66b60477c7b22b9112cd9144639083c89
Igor Šarčević <igor@renderedtext.com>
2016-07-20 14:39:09 +0200

    List.wrap implementation

---
+++ lib/dark_magic/list.ex
@@ -0,0 +1,23 @@
+defmodule DarkMagic.List do
+  import Extension
+  extends List
+
+  @doc ~S"""
+  Wraps an element into a list if it is not already a list.
+
+  # Examples
+      iex> DarkMagic.List.wrap(3)
+      [3]
+
+      iex> DarkMagic.List.wrap([1, 2])
+      [1, 2]
+  """
+  def wrap(element) when is_list(element) do
+    element
+  end
+
+  def wrap(element) when not is_list(element) do
+    [element]
+  end
+
+end
