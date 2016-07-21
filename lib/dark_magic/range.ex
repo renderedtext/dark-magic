3a3ec8fc349fe50f83b6c4120915baf657df79d1
Igor Šarčević <igor@renderedtext.com>
2016-07-21 18:42:17 +0200

    Range intersections

--- lib/dark_magic/range.ex
+++ lib/dark_magic/range.ex
@@ -19,4 +19,27 @@ defmodule DarkMagic.Range do
     Enum.min(range_a) < Enum.max(range_b) &&
     Enum.min(range_b) < Enum.max(range_a)
   end
+
+
+  @doc ~S"""
+  Returns the intersection of two ranges or nil if the intersection doesn't exists.
+
+  # Examples
+      iex> DarkMagic.Range.intersection(11..15, 12..17)
+      12..15
+
+      iex> DarkMagic.Range.intersection(11..15, 17..19)
+      nil
+  """
+  def intersection(range_a, range_b) do
+    from = max(Enum.min(range_a), Enum.min(range_b))
+    to   = min(Enum.max(range_a), Enum.max(range_b))
+
+    if from < to do
+      from..to
+    else
+      nil
+    end
+  end
+
 end
